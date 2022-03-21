8f92ab804a9498ce2bb93f3ae40f52d6828776a2
georgelabrys <100436759+georgelabrys@users.noreply.github.com>
2022-03-21 14:55:09 +0000

    added models

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/category_item.dart';
+
+import './dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   //these are used for when we use the push navigator in category_item page
@@ -17,6 +20,16 @@ class CategoryMealsScreen extends StatelessWidget {
         ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs["title"];
     final categoryId = routeArgs["id"];
+
+    //we filter our categories here, we are checking where a meal is re
+    //we refer to our dummy meals and now they can be filtered with the where iterable to a list
+    //which only contain elements of meal with a condition that we pass in our function
+    //this condition receives evey item and returns true if the meal that we are looking at has the categoryId included in its categories
+    //and we do that since categories is a list and we can use the contains method
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
         //used  with the  push navigation
@@ -25,11 +38,11 @@ class CategoryMealsScreen extends StatelessWidget {
           categoryTitle,
         ),
       ),
-      body: Center(
-        child: Text(
-          "The recipes for the Category!",
-          style: Theme.of(context).textTheme.subtitle1,
-        ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
