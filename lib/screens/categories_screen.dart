9e7fd8442165b70b7afda9e1dba8faa14350722e
georgelabrys <100436759+georgelabrys@users.noreply.github.com>
2022-03-22 14:32:42 +0000

    configured top tabbar

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -8,31 +8,26 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Delimeal"),
-      ),
-      body: GridView(
-        //we need to add some padding around the gridView
-        padding: const EdgeInsets.all(25),
-        children: DUMMY_CATEGORIES
-            .map(
-              (catData) => CategoryItem(
-                catData.id,
-                catData.title,
-                catData.color,
-              ),
-            )
-            .toList(),
-        //this is how we define a grid view
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          //for 200 width we want 300 height
-          childAspectRatio: 3 / 2,
-          //how many distance is btw columns and rows
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
+    return GridView(
+      //we need to add some padding around the gridView
+      padding: const EdgeInsets.all(25),
+      children: DUMMY_CATEGORIES
+          .map(
+            (catData) => CategoryItem(
+              catData.id,
+              catData.title,
+              catData.color,
+            ),
+          )
+          .toList(),
+      //this is how we define a grid view
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        //for 200 width we want 300 height
+        childAspectRatio: 3 / 2,
+        //how many distance is btw columns and rows
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
     );
   }
